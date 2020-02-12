27834d028cadec81c4cc25d69096591ac1f3d505
utamovita <michalkowit@gmail.com>
2020-02-13 00:28:52 +0100

    dice

--- src/components/game/high-low/dice.js
+++ src/components/game/high-low/dice.js
@@ -2,11 +2,25 @@ import React from 'react';
 import { Button } from '@material-ui/core';
 
 const dice = () => {
+
+    const handleRoll = () => {
+        console.log(Math.floor(Math.random()*6) + 1);
+    }
+
     return (
         <div class="dice">
-            <Button color="primary" variant="contained">Roll</Button>
+            <Button color="primary" variant="contained" onClick={handleRoll}>Roll</Button>
+            <div className="roll six">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
         </div>
     );
 }
+
 
 export default dice;
